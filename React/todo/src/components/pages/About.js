ab66776645c57fc94002050c85ee52e6eb281157
arnob635 <arnob635@gmail.com>
2019-11-18 20:36:38 +0600

    React Todo

---
+++ React/todo/src/components/pages/About.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function About() {
+    return (
+        <React.Fragment>
+            <h1>About</h1>
+            <h2>blah blah blah !!!!!</h2>
+        </React.Fragment>
+
+    )
+}
